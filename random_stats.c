f0c7667814625fd81453a42e40d1bee69e092db4
Phireh <Phireh@hotmail.com>
2020-06-25 15:39:58 +0200

    First commit

---
+++ random_stats.c
@@ -0,0 +1,203 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+#ifdef DEBUG
+#define PRINTD(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#else
+#define PRINTD(fmt, ...)
+#endif
+
+#define NSECS 1000000000.0
+
+void method1(int *stats);
+void method2(int *stats);
+void method3(int *stats);
+
+int P = 100000; // pool of total points
+int X = 400;    // number of stats
+int pretty_print = 0;
+
+int main(int argc, char *argv[])
+{
+  while (1) {
+    static struct option long_options[] =
+      {
+        {"pretty-print", no_argument, &pretty_print, 1},
+        {0,0,0,0}
+      };
+    /* Parse cmd arguments if provided */
+    int c, option_index = 0;
+
+    c = getopt_long(argc, argv, "x:p:", long_options, &option_index);
+    if (c == -1) break;
+    
+    switch (c) {
+    case 'x':
+      if (optopt == 'x')
+        fprintf(stderr, "Option x needs a number\n");
+      else
+        X = atoi(optarg);
+      break;
+    case 'p':
+      if (optopt == 'p')
+        fprintf(stderr, "Option p needs a number\n");
+      else
+        P = atoi(optarg);
+      break;
+    case 0:
+    default:
+      break;
+    }
+  }
+
+  printf("X = %d, P = %d\n", X, P);
+  
+  time_t t;
+  /* Initialize stats to 0 */
+  int statblock1[X];
+  for (int i = 0; i < X; ++i) statblock1[i] = 0;
+  int statblock2[X];
+  for (int i = 0; i < X; ++i) statblock2[i] = 0;
+  int statblock3[X];
+  for (int i = 0; i < X; ++i) statblock3[i] = 0;
+  
+  srand(time(&t));
+
+  /* Maybe this is dumb, benchmarking is hard okay?
+     Doing it like https://stackoverflow.com/a/41959179 */
+  
+  struct timespec t1b, t1e, t2b, t2e, t3b, t3e;
+  
+
+  
+  printf("Running method 1...\n");
+  clock_gettime(CLOCK_MONOTONIC_RAW, &t1b);
+  method1(statblock1);
+  clock_gettime(CLOCK_MONOTONIC_RAW, &t1e);
+  printf("Time = %fs\n", (t1e.tv_nsec - t1b.tv_nsec) / NSECS + (t1e.tv_sec - t1b.tv_sec));
+
+  if (pretty_print) {
+    printf("STATBLOCK 1\n");
+    for (int i = 0; i < X; ++i) {
+      printf("Stat %d -> %d\n", i, statblock1[i]);
+    }
+    printf("---------------------\n");
+  }
+  
+  printf("Running method 2...\n");
+  
+  clock_gettime(CLOCK_MONOTONIC_RAW, &t2b);
+  method2(statblock2);
+  clock_gettime(CLOCK_MONOTONIC_RAW, &t2e);
+  printf("Time = %fs\n", (t2e.tv_nsec - t2b.tv_nsec) / NSECS + (t2e.tv_sec - t2b.tv_sec));
+
+  if (pretty_print) {
+    printf("STATBLOCK 2\n");
+    for (int i = 0; i < X; ++i) {
+      printf("Stat %d -> %d\n", i, statblock2[i]);
+    }
+    printf("---------------------\n");
+  }
+  
+  printf("Running method 3...\n");
+  
+  
+  clock_gettime(CLOCK_MONOTONIC_RAW, &t3b);
+  method3(statblock3);
+  clock_gettime(CLOCK_MONOTONIC_RAW, &t3e);
+  printf("Time = %fs\n", (t3e.tv_nsec - t3b.tv_nsec) / NSECS + (t3e.tv_sec - t3b.tv_sec));
+
+  
+  if (pretty_print) {
+    printf("STATBLOCK 3\n");
+    for (int i = 0; i < X; ++i) {
+      printf("Stat %d -> %d\n", i, statblock3[i]);
+    }
+    printf("---------------------\n");
+  }
+}
+
+/* Method 1: Give out points one by one, to the highest number of a global roll */
+void method1(int *stats)
+{
+  int r;
+  for (int i = 0; i < P; ++i) {
+    int max_rand = 0, max_idx = 0;
+    for (int j = 0; j < X; ++j) {
+      r = rand();
+      if (r > max_rand) {
+        max_rand = r;
+        max_idx = j;
+      }
+    }
+    // Note that we skip checking for all-0s. We just give a point to idx 0 in that case.
+    ++stats[max_idx];
+  }
+}
+
+/* Method 2: Give out points in P/X chunks with local rolls, then give out the rest using Method 1 */
+void method2(int *stats)
+{
+  int p = P;
+  int chunk_size = P/X;
+  int r;
+
+  while (p && chunk_size > 1) {
+    PRINTD("p = %d, chunk_size = %d\n", p, chunk_size);
+    for (int i = 0; i < X; ++i) {
+      r = rand();
+      stats[i] += r % chunk_size;
+      p -= r % chunk_size;
+      PRINTD("Giving %d points to idx %d\n", r % chunk_size, i);
+    }
+    chunk_size = p/X;
+  }
+
+  /* Revert back to Method 1 */
+  for (int i = 0; i < p; ++i) {
+    int max_rand = 0, max_idx = 0;
+    for (int j = 0; j < X; ++j) {
+      r = rand();
+      if (r > max_rand) {
+        max_rand = r;
+        max_idx = j;
+      }
+    }
+    ++stats[max_idx];
+    --p;
+    PRINTD("Giving 1 point to idx %d\n", max_idx);
+  }
+}
+
+/* Method 3: Give out a point with a global roll with each iteration after giving out P/X chunks with local rolls that same iteration */
+void method3(int *stats)
+{
+  int p = P;
+  int chunk_size = P/X;
+  int r;
+
+  while (p) {
+    int max_rand = 0, max_idx = 0;
+      chunk_size = p/X;
+      PRINTD("p = %d, chunk_size = %d\n", p, chunk_size);
+      for (int j = 0; j < X; ++j) {
+        r = rand();
+        /* Method 2: Give out a p/X chunk */
+        if (chunk_size > 1) {
+          stats[j] += r % chunk_size;
+          p -= r % chunk_size;
+          PRINTD("Giving %d points to idx %d\n", r % chunk_size, j);
+        }
+        /* Method 1: Give out 1 point */
+        if (r > max_rand) {
+          max_rand = r;
+          max_idx = j;
+        }
+      }
+    ++stats[max_idx];
+    --p;
+    PRINTD("Giving 1 point to idx %d\n", max_idx);
+  }
+}
